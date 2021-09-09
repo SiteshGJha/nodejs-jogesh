aa267121d91f60abc14fc8eaf62bda69297cfb93
Sitesh Jha <sitesh.jha@visiblealpha.com>
2021-09-09 16:45:08 +0530

    express rest api with mongoose implementation

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -6,7 +6,19 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-// var dishRouter = req
+var dishRouter = require('./routes/dishRouter');
+
+const mongoose = require('mongoose')
+const dishes = require('./models/dishes');
+const url = 'mongodb://localhost:27017/conFusion';
+
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+  console.log('Connected correctly to server')
+}, (err) => { console.log(err)
+});
+
 var app = express();
 
 // view engine setup
@@ -21,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/dishes', dishRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
